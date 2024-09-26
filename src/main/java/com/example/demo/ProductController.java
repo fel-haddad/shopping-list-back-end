41e916058fe9fedcdac56379b807f21c3991133c
Farid El-Haddad <contact@el-haddad.com>
2024-09-26 20:07:48 +0200

    Add CORS configuration

--- src/main/java/com/example/demo/ProductController.java
+++ src/main/java/com/example/demo/ProductController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:8100")
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/products")
 public class ProductController {
 
